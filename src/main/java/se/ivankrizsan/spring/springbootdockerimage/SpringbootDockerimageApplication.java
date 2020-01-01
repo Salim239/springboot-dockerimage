c8e66844fff50269933c4d7417e041d73ceb94e8
Salim Hamidi <salim.hamidi@gmail.com>
2020-01-01 22:30:21 +0100

    First commit

---
+++ src/main/java/se/ivankrizsan/spring/springbootdockerimage/SpringbootDockerimageApplication.java
@@ -0,0 +1,13 @@
+package se.ivankrizsan.spring.springbootdockerimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerimageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/ivankrizsan/spring/springbootdockerimage/SpringbootDockerimageApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ivankrizsan.spring.springbootdockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
